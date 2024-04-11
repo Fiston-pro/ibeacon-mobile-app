c4ec4bf70b33d01eb2cf96c3c213ba9dcb32a501
Fiston <60503084+Fiston-pro@users.noreply.github.com>
2024-04-11 20:34:20 +0200

    Added a way to connect to wifi and host a page

--- esp32_ibeacon.cpp
+++ esp32_ibeacon.cpp
@@ -3,6 +3,8 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include <BLEBeacon.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
 #define DEVICE_NAME            "ESP32"
 #define SERVICE_UUID           "7A0247E7-8E88-409B-A959-AB5092DDB03E"
@@ -14,6 +16,8 @@ BLEServer *pServer;
 BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
 uint8_t value = 0;
+
+WebServer server(80);
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -46,10 +50,16 @@ class MyCallbacks: public BLECharacteristicCallbacks {
         Serial.println();
         Serial.println("***");
 
+        // Here you would change the iBeacon name based on the received value
+        // For simplicity, let's assume the received value is the new name
+        BLEBeacon::setBeaconName(rxValue.c_str());
       }
     }
 };
 
+void handleRoot() {
+  server.send(200, "text/html", "<input type='text' name='new_name'><button type='submit'>Submit</button>");
+}
 
 void init_service() {
   BLEAdvertising* pAdvertising;
@@ -110,10 +120,24 @@ void setup() {
   init_service();
   init_beacon();
 
+  // Initialize WiFi
+  WiFi.begin("SSID", "PASSWORD");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("WiFi connected");
+
+  // Start the web server
+  server.on("/", handleRoot);
+  server.begin();
+
   Serial.println("iBeacon + service defined and advertising!");
 }
 
 void loop() {
+  server.handleClient(); // Handle web server requests
+
   if (deviceConnected) {
     Serial.printf("* NOTIFY: %d *\n", value);
     pCharacteristic->setValue(&value, 1);
@@ -121,4 +145,4 @@ void loop() {
     value++;
   }
   delay(2000);
-}
+} 
